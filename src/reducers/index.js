3120d2c9264f61f0168b21f69a7a10c6be16cafa
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-12 04:23:21 +0000

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
-import catsReducer from './cats_reducer'
-import { combineReducers } from 'redux'
-
-const rootReducer = combineReducers({
-    cats: catsReducer
-  });
-
-export default rootReducer
+// import catsReducer from './cats_reducer'
+// import { combineReducers } from 'redux'
+//
+// const rootReducer = combineReducers({
+//     cats: catsReducer
+//   });
+//
+// export default rootReducer
