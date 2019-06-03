820ec21e12ff26ccb8e311fa32a87fa82d0dea1d
neoDJS <johnsteeve2002@gmail.com>
2019-06-03 14:18:44 -0400

    catlist Change default props adding

--- src/CatList.js
+++ src/CatList.js
@@ -1,15 +1,17 @@
+
 import React from 'react'
 
-const CatList = ({catPics}) => {
-  function renderCats() {
+const CatList = ({ catPics }) => {
+        function renderCats() {
+            return catPics.map(catPic => < img src = { catPic.url }
+                alt = { catPic.url }
+                />);
+            }
 
-    return catPics.map(catPic => <img src={catPic.url} alt={catPic.url}/>)
-  }
-  return (
-    <div>
-      {renderCats()}
-    </div>
-  )
-}
+            return <div > { renderCats() } < /div>;
+        };
 
-export default CatList;
+        CatList.defaultProps = {
+            catPics: []
+        }
+        export default CatList;
