07aaa74fa5b85fcb93a6c8c7aae171849097706a
Herna Michel <labellehernamichel@yahoo.fr>
2019-11-08 14:51:22 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-// import React, { Component } from 'react';
-// import {Navbar} from 'react-bootstrap'
+import React, { Component } from 'react';
+import {Navbar} from 'react-bootstrap'
+import {connect} from 'react-redux'
+import {fetchCats} from './actions/catActions'
+import CatList from './CatList'
 
-// class App extends Component {   
-  
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navbar>
-//           <Navbar.Header>
-//             <Navbar.Brand>
-//               <a href="#">CatBook</a>
-//             </Navbar.Brand>
-//           </Navbar.Header>
-//         </Navbar>
-//       </div>
-//     );
-//   }
-// }
+class App extends Component {
 
+  componentDidMount() {
+    this.props.fetchCats()
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <CatList catPics={this.props.catPics} />
+              Cats
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
-// export default App
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats.pictures
+  }
+}
 
+export default connect(mapStateToProps, {fetchCats})(App)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
- 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import catsReducer from './reducers/catsReducer.js';
- 
-const store = createStore(catsReducer, applyMiddleware(thunk))
- 
+import ReactDOM from 'react-dom'
+import App from './App'
+import { createStore, appluMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+import { Provider } from 'react-redux'
+
+const store = createStore(rootReducer, appluMiddleware(thunk))
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
